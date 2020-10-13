d92dff0bc8d16b68c517614b0071899164f70db5
Slyke <slyke@phoxenix.com>
2020-10-13 02:39:50 -0400

    Added cli getter

---
+++ get_lite.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget https://downloads.raspberrypi.org/raspios_lite_armhf/images/raspios_lite_armhf-2020-08-24/2020-08-20-raspios-buster-armhf-lite.zip
+unzip 2020-08-20-raspios-buster-armhf-lite.zip && rm 2020-08-20-raspios-buster-armhf-lite.zip
